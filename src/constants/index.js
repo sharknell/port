f967e1b01969111e31e20e6467e5d69306be6f8c
sharknell <103021663+sharknell@users.noreply.github.com>
2023-10-25 00:45:52 +0900

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,4 +1,4 @@
-
+import "./main.css";
 
 export const headerNav = [
     {
@@ -25,32 +25,45 @@ export const skillText = [
     {
         title: "최종 학력 및 교육 과정",
         desc: (
-            <div>
-                <h3> 전공 :</h3>
-                <b> 융합소프트웨어  </b>
+            <div className="education">
+                <div className="section">
+                    <h3>전공:</h3>
+                    <b>융합소프트웨어</b>
+                </div>
                 <hr />
-
-                <h3> 학력 :</h3>
-                <b> 평택대학교</b> 2018.03 ~ 2023.02 (졸업) <br />
-                <b> 의정부 공업 고등학교 </b> 스마트전자과 2015.03 ~ 2018.2 (졸업) <br />
+                <div className="section">
+                    <h3>학력:</h3>
+                    <b>평택대학교</b> 2018.03 ~ 2023.02 (졸업) <br />
+                    <b>의정부 공업 고등학교</b> 스마트전자과 2015.03 ~ 2018.2 (졸업) <br />
+                </div>
                 <hr />
-
-                <h3 > 교육 과정 :</h3>
-                <b>(기업수요)Open API 를 활용한 Java기반 풀스택 개발자 양성 과정</b> <br />
-                2023.04 ~ 2023.10
+                <div className="section">
+                    <h3>교육 과정:</h3>
+                    <b>(기업수요)Open API를 활용한 Java기반 풀스택 개발자 양성 과정</b> <br />
+                    2023.04 ~ 2023.10
+                </div>
                 <hr />
-
-                <h3> 기술 스택</h3>
-                <h4> Front End</h4>
-                <b>HTML5 &ensp; CSS3 &ensp; JAVASCRIPT &ensp; REACT</b><br />
-                <h4> BACK End</h4>
-                <b>JAVA &ensp; SPRINGBOOT &ensp; MARIADB &ensp; MYSQL &ensp;</b>
-                <h4> TOOL </h4>
-                <b> IntelliJ IDEA &ensp; VSCODE &ensp; Postman &ensp; GITHUB &ensp;</b>
+                <div className="section">
+                    <h3>기술 스택:</h3>
+                    <div className="subsection">
+                        <h4>Front End</h4>
+                        <b>HTML5 &ensp; CSS3 &ensp; JAVASCRIPT &ensp; REACT</b><br />
+                    </div>
+                    <div className="subsection">
+                        <h4>Back End</h4>
+                        <b>JAVA &ensp; SPRINGBOOT &ensp; MARIADB &ensp; MYSQL &ensp;</b>
+                    </div>
+                    <div className="subsection">
+                        <h4>Tool</h4>
+                        <b>IntelliJ IDEA &ensp; VSCODE &ensp; Postman &ensp; GITHUB &ensp;</b>
+                    </div>
+                </div>
                 <hr />
             </div>
         ),
     },
+    // 다른 skillText 항목들...
+
     {
         title: "성장 과정.",
         desc: "공동의 목표를 향해 협력하는 모습은 저에게 큰 영감을 주었습니다. " +
@@ -78,22 +91,20 @@ export const skillText = [
             "학습 경험과 프로젝트 수행을 통해 개발에 대한 기본적인 지식과 역량을 쌓았으며," +
             " 앞으로도 다양한 경험을 통해 성장해 나갈 것입니다." +
             " 지원한 직무에서는 기술적인 역량을 활용하여 문제를 해결하고, 협력을 통해 프로젝트를 성공적으로 이끌어 나가고자 합니다."
-
-
     }
-]
-
+    ]
 export const projectText = [
     {
         text: ["App", "Project" , "Android Studio", "Turtle neck helper"],
         title : "Stretching methods, alarms, and camera functions are provided for users suffering from turtle neck syndrome.",
-        code: "",
-        view: "",
+        ppt : "https://docs.google.com/presentation/d/1eZJ1qnUlTbu1f5xGFqDTDUZ-UHUMW2hqLzEYb30EN_g/edit?usp=sharing",
+        position : " Team Member",
+        charge : "유사 프로젝트 분석과 차별성 아이디어 제안 , Open Source 분석 및 카메라 각도 측정 담당 ",
         info : [
             "Used Kotlin " ,
             "production period : 2022.03.02 ~ 2022.06.23",
             "use stack : kotlin, Pigma , AdobeXD",
-            "tool: Android Studio"
+            "tool: Android Studio",
         ]
     }
 ,
@@ -101,11 +112,15 @@ export const projectText = [
         text: ["Java", "Project", "Chatting Program" , "GaebalTalk"],
         title: "Chatting Program using Java GUI",
         code: "https://github.com/sharknell/ChattingPrograme",
+        view: "https://drive.google.com/file/d/1xkDT8X6w7IS5_tzt1nEA5ol-lvimJ9za/view",
+        ppt : "https://docs.google.com/presentation/d/13BepA_d7lHwMD-8gbg_K65ro6FtMsWjkcf3Hipvt10c/edit?usp=sharing",
+        position: " PM(Project Manager)",
+        charge :"Notion 을 활용한 일정 관리 및 회의, Weather API(날씨 API) 활용 및 형상 관리",
         info: [
             "Increased Java Language Competency",
             "production period : 2023.07.23 - 2023.08.01",
             "use stack : Java 1.8 , Used Java GUI(Swing)",
-            "tool : IntelliJ(Me), Eclipse(Team Members), GitHub(형상 관리)"
+            "tool : IntelliJ(Me), Eclipse(Team Members), GitHub(형상 관리)",
         ],
     },
 
@@ -113,6 +128,9 @@ export const projectText = [
         text: ["Web", "Project", "Online Shopping Mall"],
         title: "Web Project with Springboot",
         code: "https://github.com/sharknell/Website",
+        ppt : "https://docs.google.com/presentation/d/105c6f35L9HGQR4yqfremxD_ftdtlu4NleHikqzCSpGM/edit?usp=sharing",
+        position:" PM(Project Manager)",
+        charge: "웹 개발 기획 , Thymeleaf 을 사용한 상품들 View 로 구현 ,Cart(장바구니)",
         info: [
             "Understanding Spring Boot structure and capabilities",
             "production period : 2023.09.08 - 2023.10.05",
@@ -123,15 +141,15 @@ export const projectText = [
     {
         text: ["Web" , "Portfolio" , "My Profile"],
         title: "My Portfolio with React",
-        code: "",
-        view: "",
+        position: " Personal Project",
+        charge: "WebStroy Template Used And Change My Portfolio",
         info: [
             "About Me",
-            "production period : two days",
-            "use stack : HTML5/CSS3, CSS Variable, react",
+            "production period : One Week",
+            "use stack : REACT",
             "tool : IntelliJ , Visual Studio Code(VScode)"
         ],
-    }
+    },
 ];
 export const contactText = [
     {
